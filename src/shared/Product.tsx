3065fde9bc6f28dd943ed877f4d7996c4a8aadc2
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2017-11-30 16:14:07 +0900

    with TouchableOpacity

--- src/shared/Product.tsx
+++ src/shared/Product.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text, Image, ImageRequireSource } from "react-native";
+import { View, Text, TouchableOpacity, Image, ImageRequireSource } from "react-native";
 
 export interface Props {
   source: ImageRequireSource;
@@ -7,10 +7,12 @@ export interface Props {
 }
 
 export default (props: Props) => (
-  <View style={{ alignItems: "center" }}>
-    <View style={{ alignItems: "center", borderRadius: 4, borderWidth: 0.5, borderColor: "#AAA", marginVertical: 4, width: 200, paddingVertical: 8 }}>
-      <Image source={props.source} style={{ width: 100, height: 100 }} />
-      <Text>{props.title}</Text>
+  <TouchableOpacity>
+    <View style={{ alignItems: "center" }}>
+      <View style={{ alignItems: "center", borderRadius: 4, borderWidth: 0.5, borderColor: "#AAA", marginVertical: 4, width: 200, paddingVertical: 8 }}>
+        <Image source={props.source} style={{ width: 100, height: 100 }} />
+        <Text>{props.title}</Text>
+      </View>
     </View>
-  </View>
+  </TouchableOpacity>
 );
