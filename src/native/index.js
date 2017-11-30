ef2241cec8213ee89ccd2296a9b7f5e5dd4601b2
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2017-11-30 13:15:07 +0900

    fix native app build environment

--- src/native/index.js
+++ src/native/index.js
@@ -1,1 +1,7 @@
 import "./App";
+
+/* If you use 'react-native init' app, set below code here. */
+// import { AppRegistry } from 'react-native';
+// import App from './App';
+//
+// AppRegistry.registerComponent('MyAwesomeApp', () => App);
