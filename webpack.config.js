5e2a0d9fb011db31c21df5099ee8cd69f8d28340
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2017-11-30 15:04:22 +0900

    replace babel with typescript (allowJs)

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,22 @@
 const path = require("path");
 const webpack = require("webpack");
 
-// This is needed for webpack to compile JavaScript.
-// Many OSS React Native packages are not compiled to ES5 before being
-// published. If you depend on uncompiled packages they may cause webpack build
-// errors. To fix this webpack can be configured to compile to the necessary
-// `node_module`.
-const babelLoaderConfiguration = {
-  test: /\.js$/,
-  // Add every directory that needs to be compiled by Babel during the build
-  include: [
-    path.resolve(__dirname, 'src'),
-    path.resolve(__dirname, 'node_modules/react-native-uncompiled')
+const tsLoaderConfiguration = {
+  test: /\.(tsx?|js)$/,
+  exclude: [
+    "/node_modules/"
   ],
   use: {
-    loader: 'babel-loader',
+    loader: 'ts-loader',
     options: {
-      cacheDirectory: true,
-      // This aliases 'react-native' to 'react-native-web' and includes only
-      // the modules needed by the app
-      plugins: ['react-native-web/babel'],
-      // The 'react-native' preset is recommended (or use your own .babelrc)
-      presets: ['react-native']
+      compilerOptions: { // overwrite tsconfig.json
+        target: "ES5",
+        jsx: "react",
+        outDir: "web"
+      }
     }
   }
-};
+}
 
 // This is needed for webpack to import static images in JavaScript files
 const fileLoaderConfiguration = {
@@ -50,7 +42,7 @@ module.exports = {
   },
   module: {
     rules: [
-      babelLoaderConfiguration,
+      tsLoaderConfiguration,
       fileLoaderConfiguration
     ]
   },
@@ -68,6 +60,9 @@ module.exports = {
     // If you're working on a multi-platform React Native app, web-specific
     // module implementations should be written in files using the extension
     // `.web.js`.
-    extensions: [ '.web.js', '.js' ]
+    extensions: [ '.web.js', '.js' ],
+    alias: {
+      'react-native': 'react-native-web'
+    }
   }
 }
