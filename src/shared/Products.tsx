06d71393f7fea1dbdcd10ed560099c33cf3cc4ab
Yukiya Nakagawa <y.nakagawa@water-cell.jp>
2017-12-12 19:28:31 +0900

    JetBrains product pages are linked

--- src/shared/Products.tsx
+++ src/shared/Products.tsx
@@ -1,35 +1,42 @@
 import * as React from "react";
-import { View } from "react-native";
+import { View, Linking } from "react-native";
 import Product from "./Product";
 import Sound from "./Sound";
 
-const products = [
-  { name: "AppCode", source: require("../../assets/images/appcode.png") },
-  { name: "CLion", source: require("../../assets/images/clion.png") },
-  { name: "DataGrip", source: require("../../assets/images/datagrip.png") },
-  { name: "GoLand", source: require("../../assets/images/goland.png") },
-  { name: "IntelliJ IDEA", source: require("../../assets/images/intellij_idea.png") },
-  { name: "PhpStorm", source: require("../../assets/images/phpstorm.png") },
-  { name: "PyCharm", source: require("../../assets/images/pycharm.png") },
-  { name: "ReSharper C++", source: require("../../assets/images/resharper_cplusplus.png") },
-  { name: "ReSharper", source: require("../../assets/images/resharper.png") },
-  { name: "Rider", source: require("../../assets/images/rider.png") },
-  { name: "RubyMine", source: require("../../assets/images/rubymine.png") },
-  { name: "WebStorm", source: require("../../assets/images/webstorm.png") },
+const products: Product[] = [
+  { name: "AppCode", source: require("../../assets/images/appcode.png"), url: "http://www.jetbrains.com/objc/" },
+  { name: "CLion", source: require("../../assets/images/clion.png"), url: "http://www.jetbrains.com/clion/" },
+  { name: "DataGrip", source: require("../../assets/images/datagrip.png"), url: "http://www.jetbrains.com/datagrip/" },
+  { name: "GoLand", source: require("../../assets/images/goland.png"), url: "http://www.jetbrains.com/go/" },
+  { name: "IntelliJ IDEA", source: require("../../assets/images/intellij_idea.png"), url: "http://www.jetbrains.com/idea/" },
+  { name: "PhpStorm", source: require("../../assets/images/phpstorm.png"), url: "http://www.jetbrains.com/phpstorm/" },
+  { name: "PyCharm", source: require("../../assets/images/pycharm.png"), url: "http://www.jetbrains.com/pycharm/" },
+  { name: "ReSharper C++", source: require("../../assets/images/resharper_cplusplus.png"), url: "http://www.jetbrains.com/resharper-cpp/" },
+  { name: "ReSharper", source: require("../../assets/images/resharper.png"), url: "http://www.jetbrains.com/resharper/" },
+  { name: "Rider", source: require("../../assets/images/rider.png"), url: "http://www.jetbrains.com/rider/" },
+  { name: "RubyMine", source: require("../../assets/images/rubymine.png"), url: "http://www.jetbrains.com/ruby/" },
+  { name: "WebStorm", source: require("../../assets/images/webstorm.png"), url: "http://www.jetbrains.com/webstorm/" },
 ];
+
+interface Product {
+  name: string;
+  source: any;
+  url: string;
+}
 
 export default class Products extends React.Component<any> {
 
   sound = new Sound();
 
-  async onPress() {
-    this.sound.play();
+  async onPress(product: Product) {
+    await this.sound.play();
+    await Linking.openURL(product.url);
   }
 
   render() {
     return (
       <View style={{ flex: 1, flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around" }}>
-        { products.map( product => <Product key={product.name} title={product.name} source={product.source} onPress={() => this.onPress()} />) }
+        { products.map( product => <Product key={product.name} title={product.name} source={product.source} onPress={() => this.onPress(product)} />) }
       </View>
     );
   }
